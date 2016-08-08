f8748ecc5238e5942d7e3341b8ea4c2b4911bf7f
yansuan <89984142@qq.com>
2016-08-08 17:54:22 +0800

    Add files via upload

---
+++ aes.go
@@ -0,0 +1,68 @@
+package pongo2
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"bytes"
+)
+
+type Aes struct{}
+
+func NewAes() *Aes {
+	return &Aes{}
+}
+
+func (this *Aes) Encrypt(origData, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	origData = this.pkcs5Padding(origData, blockSize)
+	// origData = ZeroPadding(origData, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	crypted := make([]byte, len(origData))
+	// 根据CryptBlocks方法的说明，如下方式初始化crypted也可以
+	// crypted := origData
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+func (this *Aes) Decrypt(crypted, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(crypted))
+	// origData := crypted
+	blockMode.CryptBlocks(origData, crypted)
+	origData = this.pkcs5UnPadding(origData)
+	// origData = ZeroUnPadding(origData)
+	return origData, nil
+}
+
+func (this *Aes) zeroPadding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext) % blockSize
+	padtext := bytes.Repeat([]byte{0}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func (this *Aes) zeroUnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length - 1])
+	return origData[:(length - unpadding)]
+}
+
+func (this *Aes) pkcs5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext) % blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func (this *Aes) pkcs5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	// 去掉最后一个字节 unpadding 次
+	unpadding := int(origData[length - 1])
+	return origData[:(length - unpadding)]
+}
